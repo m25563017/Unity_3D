0847e3367bbfe3da8b32e48d12a1659485192398
m25563017 <71679886+m25563017@users.noreply.github.com>
2020-12-15 15:59:49 +0800

    對話程式

---
+++ 3D/Assets/script/NPC.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class NPC : MonoBehaviour
+{
+    [Header("NPC資料")]
+    public NPCdata data;
+    [Header("對話框")]
+    public GameObject dialog;
+    [Header("對話內容")]
+    public Text textContent;
+
+    public bool playerInArea;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "小名")
+        {
+            playerInArea = true;
+            Dialog();
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {  if(other.name=="小名")
+        playerInArea=false;
+      }
+    //private void Dialog()
+    //{
+    //    for (int i = 0; i < 65; i++)
+    //    {
+    //        print("我是迴圈：" + i);
+    //    }
+    //}
+    private void Dialog()
+    {
+        for (int i = 0; i < data.dialougA.Length; i++)
+        {
+            print(data.dialougA[i]);
+        }
+    }
+}
---
+++ 3D/Assets/script/NPCdata.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName ="NPC資料",menuName="Magic Banana/NPC資料")]
+
+public class NPCdata : ScriptableObject
+{
+    [Header("第一段對話"),TextArea(1,5)]
+    public string dialougA;
+    [Header("第二段對話"),TextArea(1, 5)]
+    public string dialougB;
+    [Header("第三段對話"),TextArea(1, 5)]
+    public string dialougC;
+    [Header("任務項目需求數量")]
+    public int count;
+    [Header("已經取得項目數量")]
+    public int countCurrent;
+
+}
